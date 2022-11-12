cad0febc38a2608de9568c662fd49227133af7a3
EvgeniiPetrov-UU <YOKoSo.anon@yandex.ru>
2022-11-12 22:27:23 +0800

    All tests passed

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String message = null;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
